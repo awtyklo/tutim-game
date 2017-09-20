1ca311d5977426f27de0fd86bc31dca409d1da08
Mateusz Choma <chomamateusz@gmail.com>
2017-09-20 11:07:34 +0200

    Przygotowanie pierwszych klas Obstacle i Stick

--- js/project.js
+++ js/project.js
@@ -1,14 +1,66 @@
-// function Person(name, surname) {
-//     this.health = 50;
-//     this.name = name;
-//     this.surname = surname;
-//     this.sayHello = function() {
-//         console.info('Greetings from ' + this.name);
-//     };
-// }
-// // Person.prototype.sayHello = function() {
-// //     console.info('Greetings from ' + this.name);
-// // };
+function Stick() {
+    this.health = 3;
+    var _position = {
+        x1: 0,
+        y1: 0,
+        x2: 0,
+        y2: 0
+    }
+}
+
+function Obstacle() {
+    var _position = {
+        x1: 0,
+        y1: 0,
+        x2: 0,
+        y2: 0
+    }
+}
+
+Obstacle.prototype.init = function () {
+    // Dodanie elementu DOM do przestrzeni gry
+    $('#game').append('<div class="obstacle"></div>');
+};
+
+Obstacle.prototype.move = function () {
+    // Wprawienie przeszkody w ruch
+    // $('#game').append('<div class="obstacle"></div>');
+};
+
+
+// Inicjalizacja naszego czlowieka
+Stick.prototype.init = function () {
+    // Dodanie elementu DOM do przestrzeni gry
+    $('#game').append('<div id="stick" class="init"></div>');
+};
+
+Stick.prototype.getHealth = function () {
+    return this.health;
+};
+
+Stick.prototype.removeHp = function () {
+    this.health = this.health - 1;
+    if (this.health <= 0) {
+        console.log('@TODO: GAME OVER');
+    }
+};
+
+// Unikanie przeszkody (kucanie)
+Stick.prototype.crouch = function () {
+    $('#stick').css('height', '15vh');
+    console.log('@TODO: crouch');
+};
+
+
+var stick = new Stick();
+// Inicjalizujemy ludzika
+stick.init();
+stick.crouch();
+
+var o1 = new Obstacle();
+o1.init();
+o1.move();
+
 // Person.prototype.sayHello = function() {
 //     console.info('Witam ' + this.name);
 // };
